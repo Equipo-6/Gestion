e961697ce0c692251e8daa113127ca0813a506c6
bauthista <bauthista.95@gmail.com>
2017-04-26 14:35:04 -0500

    hice un cambio

--- Control Escolar/Control Escolar/Program.cs
+++ Control Escolar/Control Escolar/Program.cs
@@ -16,7 +16,7 @@ namespace Control_Escolar
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login());
+            Application.Run(new Alumnos_Inscripciones());
         }
     }
 }
